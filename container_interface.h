d3c7b9ebd95860e4e3fd48c687e469cc59ba6365
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-05-30 13:21:26 +0300

    added correct module linking

--- container_interface.h
+++ container_interface.h
@@ -1,12 +1,17 @@
 ﻿//Point 2. Container interface abstract tamplate class (Queue)
 
+#ifndef CONTAINER_INTERFACE_H
+#define CONTAINER_INTERFACE_H
 
 template <typename T>
-class Container_interface
+class ContainerInterface
 {
 public:
+    virtual ~ContainerInterface() = default;
     virtual void push(const T& value) = 0;
     virtual void pop() = 0;
     virtual T top() const = 0;
     virtual bool empty() const = 0;
 };
+
+#endif
