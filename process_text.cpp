9113e54884bc6ca04d5bfa3f04ed51aa8d5bcf91
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-05-30 13:11:27 +0300

    task function is done (header and realization)

---
+++ process_text.cpp
@@ -0,0 +1,52 @@
+// Point 4. Main task (realisation)
+
+#include "queue.h"
+#include "process_text.h"
+#include <fstream>
+#include <iostream>
+
+bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+void processTextFile(const std::string& filename)
+{
+    CharQueue non_digits;
+    CharQueue digits;
+
+    std::ifstream input(filename);
+    if (!input.is_open())
+    {
+        throw std::runtime_error("Unable to open file " + filename);
+    }
+
+    char c;
+    while (input.get(c))
+    {
+        if (isDigit(c))
+        {
+            digits.push(c);
+        }
+        else
+        {
+            non_digits.push(c);
+        }
+    }
+
+    // Console output non-digits
+    while (!non_digits.empty())
+    {
+        std::cout << non_digits.front();
+        non_digits.pop();
+    }
+
+    // Console output digits
+    while (!digits.empty())
+    {
+        std::cout << digits.front();
+        digits.pop();
+    }
+
+    std::cout << std::endl;
+}
---
+++ process_text.h
@@ -0,0 +1,12 @@
+// Point 4. Main task (header)
+
+#ifndef PROCESS_TEXT_H
+#define PROCESS_TEXT_H
+
+#include <string>
+
+void processTextFile(const std::string& filename);
+
+bool isDigit(char c);
+
+#endif
